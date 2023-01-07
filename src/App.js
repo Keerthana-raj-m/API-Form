b7520c15ebc08e1f57c6e90eaf5b70dfbf050a28
sankarasoftware07 <keerthanaraj.95@gmail.com>
2023-01-07 15:16:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer ></ToastContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
